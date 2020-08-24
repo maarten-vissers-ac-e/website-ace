afa3ddc030acf6fdf82270e6984ecddd9e9bae6a
maarten-vissers-ac-e <maarten.vissers5@gmail.com>
2020-08-24 15:25:11 +0200

    V2

---
+++ assets/js/animations.js
@@ -0,0 +1,10 @@
+$("document").ready(function() {
+  var typed = new Typed('.type', {
+    strings: ['Adding Competence and Expertise'],
+    typeSpeed: 80,
+    backSpeed: 10,
+    fadeOut: true,
+  })
+
+
+});
